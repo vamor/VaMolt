cd63de8b402b27cb635bcde4ef9bfbe7f63d0761
Rueben <rueben.vanmol@bluecorner.be>
2025-08-28 22:37:46 +0200

    first vs code

---
+++ services/src/test/java/com/vamolt/DemoTest.java
@@ -0,0 +1,12 @@
+package com.vamolt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoTest {
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
