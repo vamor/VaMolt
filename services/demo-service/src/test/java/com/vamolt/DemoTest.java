e41565872275f440c840e3733a6f43292dfe1400
Rueben <rueben.vanmol@bluecorner.be>
2025-08-28 02:01:14 +0200

    Add demo-service module with dummy test

---
+++ services/demo-service/src/test/java/com/vamolt/DemoTest.java
@@ -0,0 +1,8 @@
+﻿package com.vamolt;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DemoTest {
+    @Test void dummyTest() { assertTrue(true); }
+}
